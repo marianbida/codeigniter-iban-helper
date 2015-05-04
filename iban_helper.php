6fe1bf2ba8369caf5796935cbf1a1d73bbae31e5
marianbida <marian.bida@gmail.com>
2015-05-04 17:04:37 +0300

    add len validation

--- iban_helper.php
+++ iban_helper.php
@@ -4,48 +4,15 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-if (!function_exists('Egn')) {
+if (!function_exists('IBAN')) {
 
-    function Egn($egn = null) {
+    function IBAN($iban = null) {
 
-        // check len, valid and real validation
-        if (mb_strlen($egn) !== 10 || $egn === "1111111111") {
+        // check len
+        if (mb_strlen($iban) !== 22) {
             return false;
         }
-        
-        $egnWeights = array(2, 4, 8, 5, 10, 9, 7, 3, 6);
-
-        $year = substr($egn, 0, 2);
-        $mon = substr($egn, 2, 2);
-        $day = substr($egn, 4, 2);
-        if ($mon > 40) {
-            if (!checkdate($mon - 40, $day, $year + 2000)) {
-                return false;
-            }
-        } elseif ($mon > 20) {
-            if (!checkdate($mon - 20, $day, $year + 1800)) {
-                return false;
-            }
-        } else {
-            if (!checkdate($mon, $day, $year + 1900)) {
-                return false;
-            }
-        }
-        $checksum = substr($egn, 9, 1);
-        $egnsum = 0;
-        for ($i = 0; $i < 9; $i++) {
-            $egnsum += substr($egn, $i, 1) * $egnWeights[$i];
-        }
-        $valid_checksum = $egnsum % 11;
-        if ($valid_checksum == 10) {
-            $valid_checksum = 0;
-        }
-        if ($checksum != $valid_checksum) {
-            return false;
-        }
-
-        return true;
     }
-
+	
 }
 
